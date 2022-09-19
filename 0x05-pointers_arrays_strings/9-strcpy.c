804496c49580eb323e074f561975ab7e09752c96
ramoya13 <stevramoya@gmail.com>
2022-09-19 15:08:12 -0700

    Adding 9-strcpy.c file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,12 @@ char *_strcpy(char *dest, char *src)
 {
 		int count = 0;
 
-			while (count >= 0)
-					{
-								*(dest + count) = *(src + count);
-										if (*(src + count) == '\0')
-														break;
-												count++;
-													}
-				return (dest);
+		while (count >= 0)
+		{
+			*(dest + count) = *(src + count);
+			if (*(src + count) == '\0')
+				break;
+			count++;
+		}
+		return (dest);
 }
