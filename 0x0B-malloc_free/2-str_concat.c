996f1c6863de30c8e05781e53454b603a31ba961
root <stevramoya@gmail.com>
2023-06-13 04:14:17 -0700

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,8 +3,8 @@
 
 /**
  * str_concat - concatenates two strings.
- * @s1: first string
- * @s2: second string
+ * @s1: first string.
+ * @s2: second string.
  *
  * Return: pointer of an array of chars
  */
@@ -18,7 +18,10 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0, s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
 	strout = malloc(sizeof(char) * (i + j + 1));
@@ -29,11 +32,11 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (k = 0, k < i, k++)
+	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
 
 	limit = j;
-	for (j = 0; j <= limit; k++; j++)
+	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
 
 	return (strout);
